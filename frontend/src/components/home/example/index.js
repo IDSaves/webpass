ad00927e25850e737abbf5bb18f0fe71f11e6533
Classic <Ivan2002dav@gmail.com>
2020-01-24 17:14:15 +0300

    Added example passport component

---
+++ frontend/src/components/home/example/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import randomName from "random-name";
+
+const images = (() => {
+    let r = require.context("./avatars", false, /.*\.svg$/);
+    let images = {};
+    r.keys().map((item, index) => { images[item.slice(2).split(".")[0]] = r(item); });
+    return images;
+})();
+
+const rand = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+const networks = [
+    "fab fa-facebook-square",
+    "fab fa-instagram",
+    "fab fa-snapchat-square",
+    "fab fa-youtube",
+    "fas fa-envelope-square",
+    "fab fa-github",
+    "fab fa-steam",
+    "fab fa-pinterest",
+    "fab fa-telegram"
+];
+
+const Example = () => {
+    const [id, setId] = useState(rand(1, 50));
+    const avatar = images[`${id}-male`] ? images[`${id}-male`] : images[`${id}-female`];
+    const pickedNetworks = (() => {
+        let result = [];
+        let amount = rand(1, 5);
+        for (let i = 0; i < amount; i++) {
+            let picked = rand(1, networks.length - 1);
+            result.push(networks[picked])
+            networks.splice(picked, 1);
+        }
+        return result;
+    })();
+
+    return(
+        <div className="example-passport mt-5 p-3 text-center">
+            <img src={images[`${id}-male`] ? images[`${id}-male`] : images[`${id}-female`]} className="img-fluid" alt="Avatar"/>
+            <h4 className="mt-3">{randomName.first() + " " + randomName.last()}</h4>
+
+            <div>
+                {pickedNetworks.map((item, i) => (
+                    <i className={item} key={i} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Example;
--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import EngWords from "../../words/english.json";
-import RuWords from "../../words/russian.json";
+import React, { Fragment } from "react";
+import Example from "./example";
+import words from "../../words";
 import "./index.scss";
 
 const Home = () => {
-    const tagline = localStorage.getItem("lang") == "Ru" ? RuWords.home.tagline : EngWords.home.tagline;
+    const tagline = localStorage.getItem("lang") == "Ru" ? words("ru").home.tagline : words().home.tagline;
+    
     return(
-        <div>
+        <Fragment>
             <h1 className="text-center mt-3"><b>WEB PASSPORT</b></h1>
             <h5 className="text-muted text-center">{tagline}</h5>
-        </div>
+
+            <center>
+                <Example />
+            </center>
+
+        </Fragment>
     );
 };
 
