391d72ae89fa2bff5d2d02da739f5c3f315b95de
IDSaves <Ivan2002dav@gmail.com>
2020-01-26 15:48:16 +0300

    Added footer prototype

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment } from "react";
 import Home from "./components/home";
 import Language from "./components/language";
+import Footer from "./components/footer";
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
 
     return(
         <Fragment>
+            <Language />
             <div className="row m-0">
                 <div className="col-lg-6 offset-lg-3 p-0 mt-3">
                     <div className="container-fluid p-3">
@@ -17,7 +19,7 @@ const App = () => {
                     </div>
                 </div>
             </div>
-            <Language />
+            <Footer />
         </Fragment>
     );
 };
---
+++ frontend/src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className="p-2">
+            <p className="text-center mb-0">webpass.app @2020</p>
+        </div>
+    );
+};
+
+export default Footer;
