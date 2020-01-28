52b7df130f427c73deb36fe43e69e112cbacaf6a
IDSaves <Ivan2002dav@gmail.com>
2020-01-28 23:31:15 +0300

    Component name change

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import Example from "./example";
 import Create from "./create";
 import Search from "./search";
-import Edit from "./edit";
+import Management from "./management";
 import words from "../../words";
 import "./index.scss";
 
@@ -14,10 +14,10 @@ const Home = () => {
             <h1 className="text-center mt-3"><b>WEB PASSPORT</b></h1>
             <h5 className="text-muted text-center">{tagline}</h5>
 
-            <center className="mb-5">
+            <center className="mb-4">
                 <Example />
                 <Search />
-                <Edit />
+                <Management />
                 <Create />
             </center>
 
