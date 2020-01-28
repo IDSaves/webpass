98095c1d1108282d1715c8a59484e8c9a9ab0dc5
IDSaves <Ivan2002dav@gmail.com>
2020-01-28 15:50:31 +0300

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
 import React, { useEffect, Fragment } from "react";
 import Home from "./components/home";
 import Footer from "./components/footer";
+import { Switch, Route } from "react-router";
+import { Link } from "react-router-dom";
+
+const NoMatch = ({ location }) => {
+    document.title = "404 - Davydoff's page";
+    return (
+        <div className="mt-4 mb-4 text-center">
+        <Link to="/">Home</Link>
+        <h3 className="mt-3">No match for <code>{location.pathname}</code></h3>
+        </div>
+    );
+};
 
 const App = () => {
 
@@ -19,7 +31,11 @@ const App = () => {
             <div className="row m-0">
                 <div className="col-lg-6 offset-lg-3 p-0 mt-3">
                     <div className="container-fluid">
-                        <Home />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+
+                            <Route component={NoMatch} />
+                        </Switch>
                     </div>
                 </div>
             </div>
