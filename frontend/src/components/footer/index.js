c31d5d0cd35fd1b349682f2c8d2843ecf74b5830
IDSaves <Ivan2002dav@gmail.com>
2020-01-29 16:17:36 +0300

    Added margin to footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -3,7 +3,7 @@ import Settings from "../settings";
 
 const Footer = () => {
     return(
-        <div className="p-2">
+        <div className="p-2 mt-4">
             <p className="text-center mb-0">webpass.app @2020</p>
             <Settings />
         </div>
