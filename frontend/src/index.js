83ed1e86b659dd7db3addaa41f875ef87c5ae187
IDSaves <Ivan2002dav@gmail.com>
2020-02-04 18:34:34 +0300

    Toasts provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><ToastProvider><App /></ToastProvider></Router>, document.getElementById("root"));
 
 serviceWorker.unregister();
