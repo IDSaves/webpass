cb5f0f642025a3d9145ea0f5bee7429bc2195c99
IDSaves <Ivan2002dav@gmail.com>
2020-02-08 01:18:58 +0300

    Added simple queries graphql

--- backend/graphql/resolvers/query.js
+++ backend/graphql/resolvers/query.js
@@ -1,5 +1,27 @@
+import Passports from "../../models/passports.js";
+
 export default {
-    passports: () => {
-        return "Passports"
+    passports: async () => {
+        let every = await Passports.find({});
+        return every.map((passport) => {
+            passport.key = "hidden";
+            passport.conf_email = "hidden";
+            return passport;
+        });
+    },
+
+    passport: async (parent, { code }) => {
+        let passport = await Passports.findOne({code: code});
+        if (passport) {
+            passport.key = "hidden";
+            passport.conf_email = "hidden";
+            return passport;
+        }
+        return null;
+    },
+
+    isKeyValid: async (parent, { code, key }) => {
+        let check = await Passports.findOne({code: code, key: key});
+        return Boolean(check);
     }
 }
