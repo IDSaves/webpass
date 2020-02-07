0df0990ae35231600207720a136d3835fa18d0f7
IDSaves <Ivan2002dav@gmail.com>
2020-02-07 18:11:10 +0300

    GraphQL enabled

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,24 @@ import express from "express";
 import mongoose  from "mongoose";
 import bodyParser  from "body-parser";
 import graphql  from "express-graphql";
-import { importSchema } from "graphql-import";
 import { makeExecutableSchema } from "graphql-tools";
-// import resolvers from "./graphql/resolvers";
+import resolvers from "./graphql/resolvers";
+import fs from "fs";
+import path from "path";
 require("dotenv").config();
 
 const app = express();
 
-app.use(bodyParser.json({limit: "50mb", extended: true}));
+app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
 
-// const typeDefs = importSchema("./graphql/Schema.graphql");
-// app.use("/graphql",
-//     graphql({
-//         schema: makeExecutableSchema({ typeDefs, resolvers }),
-//         graphiql: true
-//     })
-// );
+const typeDefs = fs.readFileSync("./graphql/Schema.graphql", "utf8").toString();
+app.use("/graphql",
+    graphql({
+        schema: makeExecutableSchema({ typeDefs, resolvers }),
+        graphiql: true
+    })
+);
 
 app.listen(4000, () => {
     mongoose.connect("mongodb://localhost:27017/webpass", {
