3d30aecef7101c64710e15c5819a826040225315
IDSaves <Ivan2002dav@gmail.com>
2020-02-09 17:31:57 +0300

    Passport search functionality

--- frontend/src/components/home/search/index.js
+++ frontend/src/components/home/search/index.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import words from "../../../words";
 
 const Search = () => {
+    const [ code, setCode ] = useState("");
     const text = words().home.search_input;
+
     return(
         <div className="home-component box p-3 mb-3 mt-5">
             <div className="input-group">
-                <input type="text" className="form-control" placeholder={text} />
+                <input type="text" className="form-control" placeholder={text} onChange={(e) => setCode(e.target.value)} />
                 <div className="input-group-append">
-                    <button className="btn btn-secondary" type="submit">
+                    <button className="btn btn-secondary" type="submit" onClick={() => window.location.href = `/passport/${code}`}>
                         <i className="fas fa-search" />
                     </button>
                 </div>
