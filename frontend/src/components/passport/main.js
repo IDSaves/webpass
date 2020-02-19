0140973ade63af34e659698131acb05692ce3da4
IDSaves <Ivan2002dav@gmail.com>
2020-02-19 15:07:52 +0300

    Passport avatar modal component

--- frontend/src/components/passport/main.js
+++ frontend/src/components/passport/main.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ModalImage from "react-modal-image";
 
 const Main = ({ state }) => {
     return (
         <div className="box pass-comp mt-5 mb-3 p-3">
-            <img className="mb-3" alt="avatar" src={state.avatar} />
+            <ModalImage
+                small={state.avatar}
+                medium={state.avatar}
+                hideDownload="false"
+                hideZoom="false"
+                alt="Avatar"
+                className="avatar mb-3"
+            />
             <h5 className={`${(!state.personal.name && !state.personal.surname) && "mb-0"} text-center`}>{state.personal.nickname}</h5>
             {state.personal.name || state.personal.surname ? (
                 <h6 className="text-muted mb-0 text-center">{`${state.personal.name} ${state.personal.surname}`}</h6>
