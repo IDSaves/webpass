627920fbf46b5cfca8fc8ccc277e78f61cfa70b5
IDSaves <Ivan2002dav@gmail.com>
2020-03-01 00:25:12 +0300

    Cookies notification fix and new favicon

--- frontend/src/components/cookies/notification.js
+++ frontend/src/components/cookies/notification.js
@@ -5,10 +5,15 @@ import "./index.scss";
 const Notification = () => {
     const text = words().cookies_notification;
 
+    const acceptCookie = () => {
+        localStorage.setItem('cookies_accept', 'Yes');
+        window.location.reload();
+    }
+
     return (
         <div className="box cookies-notification text-center p-2 mb-5 rounded-0">
             <h5 className="mb-0">
-                {text.first} {text.second} <a className="text-primary" href="/cookies-policy">{text.here}</a>. {text.third} <a className="text-primary" href="/" onClick={() => localStorage.setItem('cookies_accept', 'Yes')}>{text.ok}</a>
+                {text.first} {text.second} <a className="text-primary" href="/cookies-policy">{text.here}</a>. {text.third} <a className="text-primary" href="#" onClick={acceptCookie}>{text.ok}</a>
             </h5>
         </div>
     );
